9551287df3d143a1355189efb2d10e6121a0c1e9
jiazhanqiu <1760286818@qq.com>
2020-05-01 12:13:06 +0800

    提交修改2

--- Student.java
+++ Student.java
@@ -1,6 +1,6 @@
 public class Student{
 	private Integer sid;	//Die001
-	private String name;	//edit @163.com
+	private String name;	//edit @163.com,20200501
 	
 	public void setName(String name){
 		this.name = name;
